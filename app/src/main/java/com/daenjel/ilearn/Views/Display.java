a86952e537d3991dd08db31dcfc232fa72265055
Daenjel <daniel.mbugua@atlancis.com>
2019-05-22 16:05:55 +0300

    creating a toggle button for changing device orientation during content display

--- app/src/main/java/com/daenjel/ilearn/Views/Display.java
+++ app/src/main/java/com/daenjel/ilearn/Views/Display.java
@@ -2,22 +2,30 @@ package com.daenjel.ilearn.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.daenjel.ilearn.R;
 
 public class Display extends AppCompatActivity {
 
     WebView webView;
+    ToggleButton zoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
         webView = findViewById(R.id.contentDisplay);
+        zoom = findViewById(R.id.zoomer);
 
        try {
            webView.loadUrl("file:///android_asset/www/index.html");
@@ -30,5 +38,18 @@ public class Display extends AppCompatActivity {
        }catch (Exception e){
            e.printStackTrace();
        }
+
+       zoom.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if (zoom.isChecked()){
+                   setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+               }else if (!zoom.isChecked()){
+                   setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+               }else {
+                   Toast.makeText(getApplicationContext(), "Orientation", Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
     }
 }
--- app/src/main/java/com/daenjel/ilearn/Views/Sub_Strands.java
+++ app/src/main/java/com/daenjel/ilearn/Views/Sub_Strands.java
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.daenjel.ilearn.Interface.ClickListener;
@@ -39,13 +41,22 @@ public class Sub_Strands extends AppCompatActivity {
         strandsView.addOnItemTouchListener(new RecyclerTouchListener(this, strandsView, new ClickListener() {
             @Override
             public void onClick(View view, int position) {
-                //startActivity(new Intent(getApplicationContext(), Slides.class));
-                Toast.makeText(getApplicationContext(), "Sub Strand "+position, Toast.LENGTH_SHORT).show();
+                View dialog = LayoutInflater.from(Sub_Strands.this).inflate(R.layout.slider,null,false);
+                Button start = dialog.findViewById(R.id.btnSlider);
                 AlertDialog.Builder slideDialog = new AlertDialog.Builder(Sub_Strands.this);
                 slideDialog.setIcon(R.mipmap.ic_launcher);
                 slideDialog.setTitle("Choose Slide");
-                slideDialog.setView(R.layout.slider);
-                slideDialog.show();
+                slideDialog.setView(dialog);
+                final AlertDialog test = slideDialog.show();
+
+                start.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), Display.class));
+                        test.dismiss();
+                    }
+                });
+                Toast.makeText(getApplicationContext(), "Sub Strand "+position, Toast.LENGTH_SHORT).show();
             }
 
             @Override
